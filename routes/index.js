2782c7033050ba77546f60209153f6cb48bdf437
Neal Bakanas <nealbakanas@gmail.com>
2016-10-23 16:39:45 -0400

    fix this now nick

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var parse = require('csv-parse/lib/sync');
+var fs = require('fs')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Recycle App' });
+router.get('/', function(req, res, next) { 
+	fs.readFile("./data/Public_Recycling_Bins.csv", 'utf8', function(err,data) {
+	var records = parse(data, {columns: true});
+	row = records[0];
+		res.render('index', { title: 'Recycle App', data:records
+		});
+	});
+    
 });
 
 module.exports = router;
